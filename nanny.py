f38d59c596025eb23382e54c9c0db481ba00bd6b
RNordstr <RNordstrom@se.gestamp.com>
2016-05-16 17:13:01 +0200

    Now reboots on to low average hashrate

--- nanny.py
+++ nanny.py
@@ -38,8 +38,41 @@ def triggered(lines):
     
 def avg_hashrate(lines, n):
     """Calculates the average hashrate based on the n last 
-    hashing attempts"""
-    pass
+    hashing attempts
+    
+    Returnes 'None' if the number of samples is less than n
+    
+    Typical raw input:
+    i  16:31:08|main  Push: New work package received
+    i  16:31:08|main  Got work package:
+    i  16:31:08|main    Header-hash: f2e7406b47d32377cb681e12b7a5ad3aff7c8e6593f49e47899c89c34bfbd439
+    i  16:31:08|main    Seedhash: abad8f99f3918bf903c6a909d9bbc0fdfa5a2f4b9cb1196175ec825c6610126c
+    i  16:31:08|main    Target: 0000000112e0be826d694b2e62d01511f12a6061fbaec8bc02357593e70e52ba
+    i  16:31:14|main  Mining on PoWhash #f2e7406bÔÇª : 32505856 H/s = 65011712 hashes / 2 s
+    i  16:31:16|main  Mining on PoWhash #f2e7406bÔÇª : 32360234 H/s = 65011712 hashes / 2.009 s
+    i  16:31:18|main  Mining on PoWhash #f2e7406bÔÇª : 32295932 H/s = 65011712 hashes / 2.013 s
+    i  16:31:20|main  Mining on PoWhash #f2e7406bÔÇª : 31176689 H/s = 62914560 hashes / 2.018 s
+    i  16:31:22|main  Mining on PoWhash #f2e7406bÔÇª : 32279896 H/s = 65011712 hashes / 2.014 s
+    i  16:31:24|main  Mining on PoWhash #f2e7406bÔÇª : 32247873 H/s = 65011712 hashes / 2.016 s
+    i  16:31:26|main  Mining on PoWhash #f2e7406bÔÇª : 32554688 H/s = 65011712 hashes / 1.997 s
+    i  16:31:28|main  Mining on PoWhash #f2e7406bÔÇª : 31285211 H/s = 62914560 hashes / 2.011 s
+    """
+    hashrates = []
+    for line in reversed(lines):
+        if "Mining on PoWhash" in line:
+            hrs = re.search("\d+ H/s", line).group(0)
+            hr = int(hrs.split()[0])
+
+            hashrates.append(hr)
+
+            if len(hashrates) >= n:
+                break
+            
+    #If there are less samples than n, return None
+    if len(hashrates) < n:
+        return None
+
+    return sum(hashrates)/float(len(hashrates))
 
 def stuck(lines, limit = 3):
     """
@@ -53,9 +86,6 @@ def stuck(lines, limit = 3):
                 c += 1
             if "Worker stopping" in line:
                 return False
-            #Reset counter
-            #if 3 "soulution found" is found before "worker stopping"
-            #the miner can be considered dead
             if c > limit:
                 return True
 
@@ -72,6 +102,8 @@ def main():
     
     while running:
         logging.debug("Checking for errors...")
+        
+        #Read qtminer stderr-file
         try: qt_log_file = open(join(HOME_PATH, "nanny/error"), "r")
         except IOError as err: 
             logging.info(err)
@@ -89,7 +121,8 @@ def main():
             reboot("Reboot triggered: %s" % triggered(lines))
         
         #Check if average hashrate is to low (maybe we've lost a gfx-card).
-        if avg_hashrate(lines, 10) < EXPECTED_HASHRATE:
+        if (avg_hashrate(lines, 10) is not None and 
+            avg_hashrate(lines, 10) < EXPECTED_HASHRATE):
             reboot("Avg hashrate below threshold (%d), rebooting!" 
                    % avg_hashrate(lines, 10))
             
