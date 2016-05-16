5035bb1fba10dd5ba748f5043a9cf5dd1097ade3
RNordstr <RNordstrom@se.gestamp.com>
2016-05-16 17:23:34 +0200

    Updated documentation

--- nanny.py
+++ nanny.py
@@ -4,13 +4,14 @@ This script tracks the "error log" from qtminer and looks for patterns
 that suggests that the mining has stopped. If found, the computer is rebooted.
 
 In order for this to work, the stderr of qtminer has to be set to ~/nanny/error.
-> qtminer -<your usual options> 2> ~/nanny/error
+$ qtminer -<your usual options> 2> ~/nanny/error
 
 Make sure that /sbin/reboot doesn't require sudo:
-> sudo chmod u+s /sbin/reboot
+$ sudo chmod u+s /sbin/reboot
 
 Don't forget set this script to start-on-boot
 """
+
 from os import system
 from os.path import join
 from os.path import expanduser
